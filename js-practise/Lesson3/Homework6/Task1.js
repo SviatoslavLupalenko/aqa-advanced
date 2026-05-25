743ec0af4a549def5aceb714eb42d71690925c78
sviatoslav.lupalenko <lupalenko.sviatoslav@gmail.com>
2026-05-25 19:38:30 +0200

    Homework6

---
+++ js-practise/Lesson3/Homework6/Task1.js
@@ -0,0 +1,17 @@
+const averageGrade = 75
+
+if (averageGrade < 60) {
+    console.log("Bad")
+}
+else if (averageGrade <= 70) {
+    console.log("Good")
+}
+else if (averageGrade <=80) {
+    console.log("Very good")
+}  
+else if (averageGrade <= 90) {
+    console.log("Excellent")
+}
+else if (averageGrade <= 100) {
+    console.log("Outstanding")
+}
---
+++ js-practise/Lesson3/Homework6/task3.js
@@ -0,0 +1,13 @@
+let number = 5
+
+let i = 1
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} x ${i} = ${number * i}`)
+// }
+
+
+while (i <= 10) {
+    console.log(`${number} x ${i} = ${number * i}`)
+    i++
+}
